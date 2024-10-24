c22a946e5290b613e52d9618ee0863e809cf504f
SPremanandReddy <premanandreddysama@gmail.com>
2024-10-24 22:09:35 +0530

    mycompleteINVENTORYMANAGEMENTSYSTEMPROJECT

---
+++ includes/config.php
@@ -0,0 +1,20 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Offcial page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost:3307' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', 'Thulash9@' );             // Set database password
+  define( 'DB_NAME', 'inventory_system' );        // Set database name
+
+?>
